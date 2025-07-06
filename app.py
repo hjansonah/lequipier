5f22481b3e622a87d96cc9f895ecc5003e6531aa
hjansonah <bouncymediadirector@gmail.com>
2025-07-06 14:33:31 +0100

    record html

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/record')
-def record():
-    return render_template('record.html')
-
-# Optional: homepage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/record')
+@app.route('/record.html')   # Add this to serve at /record.html too
+def record():
+    return render_template('record.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
