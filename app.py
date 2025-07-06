95fb35f8b6344f48078042feefc1a3ba4129dd33
hjansonah <bouncymediadirector@gmail.com>
2025-07-06 09:49:52 +0100

    Fix missing route argument in @app.route

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route()
+@app.route("/")          # <--- Make sure the route string is inside parentheses
 def home():
-    return render_template(index.html)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
