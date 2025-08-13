6574fc69ab2b5523e1575b13cbac603aea58f74a
hjansonah <bouncymediadirector@gmail.com>
2025-08-13 12:57:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from blueprints.home import home_bp
 from blueprints.records import records_bp
+from blueprints.nwsl_appended import nwsl_appended_bp
 
 app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(home_bp)
 app.register_blueprint(records_bp)
+app.register_blueprint(nwsl_appended_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
